9f252eb4e10ec3d99edb8aba83d3ce3f84c76e71
Mihail Stoica <90096221+mihail-stoica@users.noreply.github.com>
2022-10-29 13:15:45 +0200

    Initial commit

---
+++ src/main/java/com/mihailstoica/dropandsee/DropAndSeeApplication.java
@@ -0,0 +1,13 @@
+package com.mihailstoica.dropandsee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DropAndSeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DropAndSeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mihailstoica/dropandsee/DropAndSeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mihailstoica.dropandsee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DropAndSeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
