258ff3dfe1f5673d03c4b5d3d8aa46fef7e969ef
Mihail Stoica <90096221+mihail-stoica@users.noreply.github.com>
2022-11-01 18:42:29 +0100

    Fix handleLogin api to return UserVM

--- src/main/java/com/mihailstoica/dropandsee/controller/LoginController.java
+++ src/main/java/com/mihailstoica/dropandsee/controller/LoginController.java
@@ -2,6 +2,7 @@ package com.mihailstoica.dropandsee.controller;
 
 import com.mihailstoica.dropandsee.entity.User;
 import com.mihailstoica.dropandsee.shared.CurrentUser;
+import com.mihailstoica.dropandsee.viewmodel.UserVM;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/1.0/login")
 public class LoginController {
     @PostMapping
-    User handleLogin(@CurrentUser User loggedInUser) {
-        User user = new User();
-        user.setId(loggedInUser.getId());
-        user.setUsername(loggedInUser.getUsername());
-        user.setDisplayName(loggedInUser.getDisplayName());
-        user.setImage(loggedInUser.getImage());
-        return user;
+    UserVM handleLogin(@CurrentUser User loggedInUser) {
+       return new UserVM(loggedInUser);
     }
 }
